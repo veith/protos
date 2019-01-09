5b70384a4a98989ddf50d4c5c775e31825d31db0
veith <veithz@gmail.com>
2019-01-09 20:37:37 +0100

    readme and license

---
+++ scripts/build_googleCommon.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+./scripts/build.sh color
+./scripts/build.sh date
+./scripts/build.sh dayofweek
+./scripts/build.sh latlng
+./scripts/build.sh money
+./scripts/build.sh postal_address postaladdress
+./scripts/build.sh timeofday
