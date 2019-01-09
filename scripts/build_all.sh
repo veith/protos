e3d30935b53a1e1effe9619d194dbd99569b6127
veith <veithz@gmail.com>
2019-01-09 18:18:21 +0100

    init

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
 ./scripts/build.sh rest
+./scripts/build.sh color
 ./scripts/build.sh date
+./scripts/build.sh dayofweek
+./scripts/build.sh latlng
+./scripts/build.sh money
+./scripts/build.sh postal_address postaladdress
+./scripts/build.sh timeofday
