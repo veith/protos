a141f91d7c2c514056220c20b8dac6efc26087a1
veith <veithz@gmail.com>
2019-04-25 21:59:25 +0200

    fix: use https://github.com/googleapis/googleapis/blob/master/google/rpc/error_details.proto

--- rest/rest.pb.go
+++ rest/rest.pb.go
@@ -61,7 +61,7 @@ func (x Link_Method) String() string {
 	return proto.EnumName(Link_Method_name, int32(x))
 }
 func (Link_Method) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_rest_d4b65732751dd6f2, []int{0, 0}
+	return fileDescriptor_rest_3b057f08e15bb003, []int{0, 0}
 }
 
 // IANA konforme Links /
@@ -84,7 +84,7 @@ func (m *Link) Reset()         { *m = Link{} }
 func (m *Link) String() string { return proto.CompactTextString(m) }
 func (*Link) ProtoMessage()    {}
 func (*Link) Descriptor() ([]byte, []int) {
-	return fileDescriptor_rest_d4b65732751dd6f2, []int{0}
+	return fileDescriptor_rest_3b057f08e15bb003, []int{0}
 }
 func (m *Link) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -141,85 +141,8 @@ func (m *Link) GetType() string {
 	return ""
 }
 
-// field error
-type FieldError struct {
-	// the field, where error appears
-	Field string `protobuf:"bytes,2,opt,name=field,proto3" json:"field,omitempty"`
-	// error massage
-	Message string `protobuf:"bytes,3,opt,name=message,proto3" json:"message,omitempty"`
-	// massage for developer
-	DeveloperMessage string `protobuf:"bytes,4,opt,name=developerMessage,proto3" json:"developerMessage,omitempty"`
-	// error code
-	Code                 string   `protobuf:"bytes,1,opt,name=code,proto3" json:"code,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *FieldError) Reset()         { *m = FieldError{} }
-func (m *FieldError) String() string { return proto.CompactTextString(m) }
-func (*FieldError) ProtoMessage()    {}
-func (*FieldError) Descriptor() ([]byte, []int) {
-	return fileDescriptor_rest_d4b65732751dd6f2, []int{1}
-}
-func (m *FieldError) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *FieldError) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_FieldError.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalTo(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (dst *FieldError) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_FieldError.Merge(dst, src)
-}
-func (m *FieldError) XXX_Size() int {
-	return m.Size()
-}
-func (m *FieldError) XXX_DiscardUnknown() {
-	xxx_messageInfo_FieldError.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_FieldError proto.InternalMessageInfo
-
-func (m *FieldError) GetField() string {
-	if m != nil {
-		return m.Field
-	}
-	return ""
-}
-
-func (m *FieldError) GetMessage() string {
-	if m != nil {
-		return m.Message
-	}
-	return ""
-}
-
-func (m *FieldError) GetDeveloperMessage() string {
-	if m != nil {
-		return m.DeveloperMessage
-	}
-	return ""
-}
-
-func (m *FieldError) GetCode() string {
-	if m != nil {
-		return m.Code
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*Link)(nil), "furo.type.Link")
-	proto.RegisterType((*FieldError)(nil), "furo.type.FieldError")
 	proto.RegisterEnum("furo.type.Link_Method", Link_Method_name, Link_Method_value)
 }
 func (m *Link) Marshal() (dAtA []byte, err error) {
@@ -266,51 +189,6 @@ func (m *Link) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
-func (m *FieldError) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalTo(dAtA)
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *FieldError) MarshalTo(dAtA []byte) (int, error) {
-	var i int
-	_ = i
-	var l int
-	_ = l
-	if len(m.Code) > 0 {
-		dAtA[i] = 0xa
-		i++
-		i = encodeVarintRest(dAtA, i, uint64(len(m.Code)))
-		i += copy(dAtA[i:], m.Code)
-	}
-	if len(m.Field) > 0 {
-		dAtA[i] = 0x12
-		i++
-		i = encodeVarintRest(dAtA, i, uint64(len(m.Field)))
-		i += copy(dAtA[i:], m.Field)
-	}
-	if len(m.Message) > 0 {
-		dAtA[i] = 0x1a
-		i++
-		i = encodeVarintRest(dAtA, i, uint64(len(m.Message)))
-		i += copy(dAtA[i:], m.Message)
-	}
-	if len(m.DeveloperMessage) > 0 {
-		dAtA[i] = 0x22
-		i++
-		i = encodeVarintRest(dAtA, i, uint64(len(m.DeveloperMessage)))
-		i += copy(dAtA[i:], m.DeveloperMessage)
-	}
-	if m.XXX_unrecognized != nil {
-		i += copy(dAtA[i:], m.XXX_unrecognized)
-	}
-	return i, nil
-}
-
 func encodeVarintRest(dAtA []byte, offset int, v uint64) int {
 	for v >= 1<<7 {
 		dAtA[offset] = uint8(v&0x7f | 0x80)
@@ -338,34 +216,6 @@ func (m *Link) Size() (n int) {
 		n += 1 + l + sovRest(uint64(l))
 	}
 	l = len(m.Type)
-	if l > 0 {
-		n += 1 + l + sovRest(uint64(l))
-	}
-	if m.XXX_unrecognized != nil {
-		n += len(m.XXX_unrecognized)
-	}
-	return n
-}
-
-func (m *FieldError) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	l = len(m.Code)
-	if l > 0 {
-		n += 1 + l + sovRest(uint64(l))
-	}
-	l = len(m.Field)
-	if l > 0 {
-		n += 1 + l + sovRest(uint64(l))
-	}
-	l = len(m.Message)
-	if l > 0 {
-		n += 1 + l + sovRest(uint64(l))
-	}
-	l = len(m.DeveloperMessage)
 	if l > 0 {
 		n += 1 + l + sovRest(uint64(l))
 	}
@@ -545,173 +395,6 @@ func (m *Link) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-func (m *FieldError) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowRest
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= (uint64(b) & 0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: FieldError: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: FieldError: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Code", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowRest
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthRest
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Code = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Field", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowRest
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthRest
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Field = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Message", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowRest
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthRest
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Message = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 4:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field DeveloperMessage", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowRest
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= (uint64(b) & 0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthRest
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.DeveloperMessage = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipRest(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if skippy < 0 {
-				return ErrInvalidLengthRest
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
 func skipRest(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
@@ -817,30 +500,26 @@ var (
 	ErrIntOverflowRest   = fmt.Errorf("proto: integer overflow")
 )
 
-func init() { proto.RegisterFile("rest.proto", fileDescriptor_rest_d4b65732751dd6f2) }
+func init() { proto.RegisterFile("rest.proto", fileDescriptor_rest_3b057f08e15bb003) }
 
-var fileDescriptor_rest_d4b65732751dd6f2 = []byte{
-	// 342 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x91, 0xc1, 0x4a, 0xeb, 0x40,
-	0x14, 0x86, 0xef, 0x34, 0x69, 0x72, 0x73, 0x2e, 0x5c, 0x86, 0x41, 0x24, 0x88, 0x94, 0xda, 0x55,
-	0x71, 0x91, 0x80, 0x2e, 0x5d, 0xd5, 0x36, 0x5a, 0xa1, 0xb5, 0x21, 0x8d, 0x1b, 0xc1, 0x45, 0xda,
-	0x9c, 0x26, 0xc1, 0x34, 0x13, 0x26, 0xd3, 0x82, 0xe0, 0x03, 0xba, 0xd3, 0x47, 0x90, 0x3e, 0x89,
-	0x4c, 0x12, 0xdd, 0xb8, 0x19, 0xbe, 0xf3, 0xcf, 0x7f, 0x0e, 0xf3, 0xcf, 0x01, 0x10, 0x58, 0x49,
-	0xa7, 0x14, 0x5c, 0x72, 0x66, 0x6d, 0x76, 0x82, 0x3b, 0xf2, 0xa5, 0xc4, 0x93, 0xd3, 0x84, 0xf3,
-	0x24, 0x47, 0x37, 0x2a, 0x33, 0x37, 0x2a, 0x0a, 0x2e, 0x23, 0x99, 0xf1, 0xa2, 0x6a, 0x8c, 0x83,
-	0x77, 0x02, 0xfa, 0x2c, 0x2b, 0x9e, 0x19, 0x05, 0x4d, 0x60, 0x6e, 0x93, 0x3e, 0x19, 0x5a, 0x81,
-	0x42, 0xe6, 0x80, 0xb1, 0x45, 0x99, 0xf2, 0xd8, 0xee, 0xf4, 0xc9, 0xf0, 0xff, 0xc5, 0xb1, 0xf3,
-	0x33, 0xd4, 0x51, 0x2d, 0xce, 0xbc, 0xbe, 0x0d, 0x5a, 0x17, 0x63, 0xa0, 0xa7, 0x02, 0x37, 0xb6,
-	0x56, 0x8f, 0xa8, 0x59, 0x69, 0xca, 0x6f, 0xeb, 0x8d, 0xa6, 0x78, 0xf0, 0x04, 0x46, 0xd3, 0xc9,
-	0x4c, 0xd0, 0xfc, 0x87, 0x90, 0xfe, 0x51, 0x70, 0xeb, 0x85, 0x94, 0xb0, 0xbf, 0xa0, 0x4f, 0xbd,
-	0xd1, 0x84, 0x76, 0x14, 0xf9, 0x8b, 0x65, 0x48, 0x35, 0x66, 0x41, 0xd7, 0x1f, 0x85, 0xe3, 0x29,
-	0xd5, 0x19, 0x80, 0x31, 0xf1, 0x66, 0x5e, 0xe8, 0xd1, 0xae, 0x92, 0xc3, 0x60, 0x34, 0xf6, 0xa8,
-	0xc1, 0xfe, 0x81, 0xb9, 0xf0, 0xc3, 0xbb, 0xc5, 0xfd, 0x92, 0x9a, 0x83, 0x57, 0x80, 0x9b, 0x0c,
-	0xf3, 0xd8, 0x13, 0x82, 0x0b, 0xf5, 0x80, 0x35, 0x8f, 0xb1, 0xcd, 0x55, 0x33, 0x3b, 0x82, 0xee,
-	0x46, 0x39, 0xea, 0x5c, 0x56, 0xd0, 0x14, 0xcc, 0x06, 0x73, 0x8b, 0x55, 0x15, 0x25, 0xd8, 0x26,
-	0xf8, 0x2e, 0xd9, 0x39, 0xd0, 0x18, 0xf7, 0x98, 0xf3, 0x12, 0xc5, 0xbc, 0xb5, 0x34, 0x81, 0x7e,
-	0xe9, 0xd7, 0xee, 0xdb, 0xa1, 0x47, 0x3e, 0x0e, 0x3d, 0xf2, 0x79, 0xe8, 0x91, 0xc7, 0xb3, 0x24,
-	0x93, 0xe9, 0x6e, 0xe5, 0xac, 0xf9, 0xd6, 0xdd, 0x63, 0x26, 0x53, 0xb7, 0xfe, 0xf5, 0xca, 0x55,
-	0xab, 0xba, 0x52, 0xc7, 0xca, 0xa8, 0x95, 0xcb, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x47, 0x8c,
-	0x49, 0x75, 0xbe, 0x01, 0x00, 0x00,
+var fileDescriptor_rest_3b057f08e15bb003 = []byte{
+	// 278 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x3c, 0x90, 0x41, 0x4b, 0xf3, 0x30,
+	0x1c, 0xc6, 0xdf, 0xac, 0x5d, 0xfb, 0xf6, 0x2f, 0x48, 0xc8, 0x41, 0x8a, 0x48, 0x99, 0x3b, 0xed,
+	0x94, 0x80, 0x1e, 0x3d, 0xd5, 0x2d, 0x38, 0x61, 0xda, 0xd2, 0xc5, 0x8b, 0xe0, 0xa1, 0xd3, 0xac,
+	0x2d, 0x6e, 0x4d, 0x69, 0x33, 0xc1, 0x6f, 0xe8, 0x4d, 0x3f, 0x82, 0xf4, 0x93, 0x48, 0x52, 0xf1,
+	0x12, 0x7e, 0xfc, 0xf2, 0x3c, 0x0f, 0x24, 0x00, 0xad, 0xec, 0x34, 0x6d, 0x5a, 0xa5, 0x15, 0x09,
+	0xb6, 0x87, 0x56, 0x51, 0xfd, 0xde, 0xc8, 0xd3, 0xb3, 0x42, 0xa9, 0x62, 0x27, 0x59, 0xde, 0x54,
+	0x2c, 0xaf, 0x6b, 0xa5, 0x73, 0x5d, 0xa9, 0xba, 0x1b, 0x82, 0xd3, 0x4f, 0x04, 0xee, 0xaa, 0xaa,
+	0x5f, 0x09, 0x06, 0xa7, 0x95, 0xbb, 0x10, 0x4d, 0xd0, 0x2c, 0xc8, 0x0c, 0x12, 0x0a, 0xde, 0x5e,
+	0xea, 0x52, 0xbd, 0x84, 0xa3, 0x09, 0x9a, 0x1d, 0x5f, 0x9c, 0xd0, 0xbf, 0x51, 0x6a, 0x2a, 0xf4,
+	0xce, 0xde, 0x66, 0xbf, 0x29, 0x42, 0xc0, 0x2d, 0x5b, 0xb9, 0x0d, 0x1d, 0x3b, 0x61, 0xd9, 0x38,
+	0x93, 0x0f, 0xdd, 0xc1, 0x19, 0x9e, 0x3e, 0x81, 0x37, 0x34, 0x89, 0x0f, 0x4e, 0xfa, 0x20, 0xf0,
+	0x3f, 0x03, 0x37, 0x5c, 0x60, 0x44, 0xfe, 0x83, 0xbb, 0xe4, 0xf1, 0x02, 0x8f, 0x0c, 0xa5, 0xc9,
+	0x5a, 0x60, 0x87, 0x04, 0x30, 0x4e, 0x63, 0x31, 0x5f, 0x62, 0x97, 0x00, 0x78, 0x0b, 0xbe, 0xe2,
+	0x82, 0xe3, 0xb1, 0xd1, 0x22, 0x8b, 0xe7, 0x1c, 0x7b, 0xe4, 0x08, 0xfc, 0x24, 0x15, 0xb7, 0xc9,
+	0xfd, 0x1a, 0xfb, 0xd7, 0xec, 0xa3, 0x8f, 0xd0, 0x57, 0x1f, 0xa1, 0xef, 0x3e, 0x42, 0x8f, 0xe7,
+	0x45, 0xa5, 0xcb, 0xc3, 0x86, 0x3e, 0xab, 0x3d, 0x7b, 0x93, 0x95, 0x2e, 0x99, 0x7d, 0x77, 0xc7,
+	0xcc, 0x67, 0x5d, 0x99, 0x63, 0xe3, 0x59, 0x73, 0xf9, 0x13, 0x00, 0x00, 0xff, 0xff, 0x86, 0x9f,
+	0x0a, 0xdf, 0x40, 0x01, 0x00, 0x00,
 }
